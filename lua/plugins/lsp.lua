c1f82d76de2632abbc8bd4d29131a30170b480f7
rafaelisidorio-dev <rafaelisidorio-dev.outlook.com>
2025-12-28 14:28:15 -0300

    fix: bugs in treesitter and telescope

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -217,20 +217,20 @@ return {
 			-- ts_ls = {},
 			--
 
-			lua_ls = {
+			--lua_ls = {
 				-- cmd = { ... },
 				-- filetypes = { ... },
 				-- capabilities = {},
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
+				--settings = {
+					--Lua = {
+						--completion = {
+							--callSnippet = "Replace",
+						--},
 						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
 						-- diagnostics = { disable = { 'missing-fields' } },
-					},
-				},
-			},
+					--},
+				--},
+			--},
 		}
 
 		-- Ensure the servers and tools above are installed
@@ -248,7 +248,7 @@ return {
 		-- for you, so that they are available from within Neovim.
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
-			"stylua", -- Used to format Lua code
+			--"stylua", -- Used to format Lua code
 		})
 		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+  --main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { javascript, typescript, jsx, tsx, html, css },
